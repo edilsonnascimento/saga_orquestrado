fe1bffe2748ad42fa900f7b768d682756a71fe24
Edilson do Nascimento <edilsonjoper@gmail.com>
2025-01-17 06:28:26 -0300

    ADDED directory struture and DTO.

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/History.java
@@ -0,0 +1,21 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.enums.EEventSource;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+
+    private EEventSource source;
+    private String status;
+    private String message;
+    private LocalDateTime createAt;
+}
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -0,0 +1,21 @@
+package br.com.microservices.orchestrated.orderservice.core.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    private String id;
+    private List<OrderProducts> orderProducts;
+    private String transactionId;
+    private double totalAmount;
+    private int totalItems;
+}
