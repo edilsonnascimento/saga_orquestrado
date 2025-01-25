5769fb876f3fbb8017a01a7c4058d04d80815dc5
Edilson do Nascimento <edilsonjoper@gmail.com>
2025-01-25 06:11:12 -0300

    ADDED sevice and repository for created order.

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -19,6 +20,7 @@ public class Order {
     @Id
     private String id;
     private List<OrderProducts> orderProducts;
+    private LocalDateTime createdAt;
     private String transactionId;
     private double totalAmount;
     private int totalItems;
