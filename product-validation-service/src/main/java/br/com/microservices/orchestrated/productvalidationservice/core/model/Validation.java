75a57e2ca7f50acb355a0d561168e5f93fcb8ae1
Edilson do Nascimento <edilsonjoper@gmail.com>
2025-01-28 06:32:52 -0300

    ADDED models and repository from Product.

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -0,0 +1,49 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "validation")
+public class Validation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private boolean success;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        this.createAt = now;
+        this.updateAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updateAt = LocalDateTime.now();
+    }
+}
